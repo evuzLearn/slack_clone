9db67c3374d2d55462d022e15e2af0b6b8bc73b0
evuz <jesus_jgb_45@hotmail.com>
2017-11-23 13:36:40 +0100

    Server: Create PostgreSQL models using sequelize

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import { makeExecutableSchema } from 'graphql-tools';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -18,4 +19,6 @@ app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-app.listen(8080);
+models.sequelize.sync({}).then(() => {
+  app.listen(8080);
+});
