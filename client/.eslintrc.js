b23f9b461eb521888556b267fa2aab000d999626
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-11-23 23:27:40 +0100

    Client: Setting up React with GraphQL

---
+++ client/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'react/jsx-filename-extension': 0,
+  },
+  globals: {
+    document: 1,
+  },
+};
