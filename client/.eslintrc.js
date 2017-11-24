c31372231e32b9310f7013d667a514b8592c5612
evuz <jesus_jgb_45@hotmail.com>
2017-11-24 10:55:10 +0100

    Client: Register Form in React Using Semantic UI and GraphQL

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   extends: 'airbnb',
+  "parser": "babel-eslint",
   rules: {
+    'arrow-parens': 0,
     'import/no-unresolved': 0,
     'import/extensions': 0,
     'react/jsx-filename-extension': 0,
+    "react/prop-types": 0,
   },
   globals: {
     document: 1,
