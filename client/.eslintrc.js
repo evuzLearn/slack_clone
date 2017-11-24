8c7beb2e1a8f736cc21b54143127798b0d6a6b41
evuz <jesus_jgb_45@hotmail.com>
2017-11-24 15:56:02 +0100

    Client: Login Mutation GraphQL

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   extends: 'airbnb',
-  "parser": "babel-eslint",
+  parser: 'babel-eslint',
   rules: {
     'arrow-parens': 0,
     'import/no-unresolved': 0,
     'import/extensions': 0,
     'react/jsx-filename-extension': 0,
-    "react/prop-types": 0,
+    'react/prop-types': 0,
   },
   globals: {
     document: 1,
+    localStorage: 1,
   },
 };
--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { extendObservable } from 'mobx';
 import { observer } from 'mobx-react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 import { Container, Header, Input, Button } from 'semantic-ui-react';
 
 class Login extends Component {
@@ -12,10 +14,16 @@ class Login extends Component {
     });
   }
 
-  onSubmit = () => {
+  onSubmit = async () => {
     const { email, password } = this;
-    console.log(email);
-    console.log(password);
+    const response = await this.props.mutate({
+      variables: { email, password },
+    });
+    const { ok, token, refreshToken } = response.data.login;
+    if (ok) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
+    }
   };
 
   onChange = e => {
@@ -49,4 +57,18 @@ class Login extends Component {
   }
 }
 
-export default observer(Login);
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ok
+      token
+      refreshToken
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(loginMutation)(observer(Login));
