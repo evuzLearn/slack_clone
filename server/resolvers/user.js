ecc032d571ba08026bfd622fb6a507b61394c291
evuz <jesus_jgb_45@hotmail.com>
2017-11-27 11:22:52 +0100

    Sever: Refactoring and Handling Invalid Login

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -20,25 +20,10 @@ export default {
   Mutation: {
     login: (parent, { email, password }, { models, SECRET, SECRET2 }) =>
       tryLogin(email, password, models, SECRET, SECRET2),
-    register: async (parent, { password, ...args }, { models }) => {
+    register: async (parent, args, { models }) => {
       try {
-        if (password.length < 5 || password.length > 100) {
-          return {
-            ok: false,
-            errors: [
-              {
-                path: 'password',
-                message:
-                  'The password needs to be betwen 5 and 100 characters long',
-              },
-            ],
-          };
-        }
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const user = await models.User.create({
-          ...args,
-          password: hashedPassword,
-        });
+        console.log(args);
+        const user = await models.User.create(args);
         return {
           ok: true,
           user,
